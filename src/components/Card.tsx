804aae6e98313a948bca71a894dcbd73ea084cfa
Fernando Goncalves <fernando.hmg@gmail.com>
2022-08-21 15:26:14 -0300

    improving semantics

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -14,22 +14,24 @@ export const Card = () => {
         alt="Gabrielle Parfum"
       />
       <section className="p-5 font-sans sm:flex-1 sm:p-8">
-        <p className="text-dark-grayish-blue text-xs tracking-[.4em] font-medium my-2 uppercase">
-          Perfume
-        </p>
-        <h1 className="text-dark-blue font-serif text-3xl">
-          Gabrielle Essence Eau De Parfum
-        </h1>
+        <header>
+          <p className="text-dark-grayish-blue text-xs tracking-[.4em] font-medium my-2 uppercase">
+            Perfume
+          </p>
+          <h1 className="text-dark-blue font-serif text-3xl">
+            Gabrielle Essence Eau De Parfum
+          </h1>
+        </header>
         <p className="text-dark-grayish-blue text-sm font-medium leading-6 my-5">
           A floral, solar and voluptuous interpretation composed by Olivier
           Polge, Perfumer-Creator for the House of CHANEL
         </p>
-        <strong className="text-cyan font-serif text-3xl align-middle block xs:inline">
+        <b className="text-cyan font-serif text-3xl align-middle block xs:inline">
           $149.99
-        </strong>
-        <p className="text-dark-grayish-blue line-through align-middle inline text-xs ml-5">
+        </b>
+        <s className="text-dark-grayish-blue align-middle inline text-xs ml-5">
           $169.99
-        </p>
+        </s>
         <button className="bg-cyan text-white flex justify-center items-center gap-3 w-full rounded-lg p-3 mt-3 text-sm hover:bg-dark-cyan sm:mt-7">
           <img src={cartIcon} alt="" />
           Add to Cart
